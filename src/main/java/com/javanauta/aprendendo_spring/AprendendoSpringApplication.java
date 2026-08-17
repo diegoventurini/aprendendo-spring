76f957830b839723bb02322f80d3b9b239d5ec21
diegoventurini <diegoventurini27@gmail.com>
2026-08-17 10:00:45 -0300

    "comentário na classe de execução"

--- src/main/java/com/javanauta/aprendendo_spring/AprendendoSpringApplication.java
+++ src/main/java/com/javanauta/aprendendo_spring/AprendendoSpringApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class AprendendoSpringApplication {
-	// Classe de execução
+	// Classe de execução0
 	public static void main(String[] args) {
 		 SpringApplication.run(AprendendoSpringApplication.class, args);
 
